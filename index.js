317614a72670af559f1dc93ef59aef323e47a01b
MarioSuper1234 <103045311+MarioSuper1234@users.noreply.github.com>
2024-10-17 15:54:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 36 + 1 (0) = 37 miejsc na tabeli
 
 const ruleta = document.getElementById("krecidlo");
+const kulka = document.getElementById("ball");
 var result = 0;
 const onRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
 const onBlack = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
@@ -8,6 +9,7 @@ const onBlack = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33,
 function krync() {
 	result = Math.floor(Math.random() * 37);
 	ruleta.className = "spin";
+	kulka.className = "spinBall";
 
 	setTimeout(shit, 5000);
 }
